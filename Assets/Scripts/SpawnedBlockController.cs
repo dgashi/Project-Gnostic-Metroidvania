7f5cbd981c14867454b28e2920e5228dcb8756bd
dgashi <don.gashi@hotmail.com>
2020-01-21 17:13:26 +0200

    Replaced NPC_Turret script
    Added a third-party script to control turret rotation and wrote a separate script for turret shooting and target seeking behaviour, replacing old NPC_Turret script that was causing randomly jittering turret rotation bug.

--- Assets/Scripts/SpawnedBlockController.cs
+++ Assets/Scripts/SpawnedBlockController.cs
@@ -8,13 +8,17 @@ public class SpawnedBlockController : FallingObjectController
     private bool isInvincible = false;
     [SerializeField]
     private bool pairedToPlatform;
+    private HashSet<Rigidbody2D> passengerrbs;
     private SpriteRenderer sr;
+    private GameObject platformConnectedTo;
+    private PlatformController platformControllerBelow;
 
     public override void Start()
     {
         base.Start();
         gameObject.SetActive(false);
         sr = GetComponent<SpriteRenderer>();
+        passengerrbs = new HashSet<Rigidbody2D>();
     }
 
     public override void Update()
@@ -32,15 +36,35 @@ public class SpawnedBlockController : FallingObjectController
 
         if (belowInput.collidingObjects.Count != 0 && !pairedToPlatform)
         {
-            sr.sortingLayerID = belowInput.collidingObjects[0].GetComponent<TilemapRenderer>().sortingLayerID;
-            sr.sortingOrder = belowInput.collidingObjects[0].GetComponent<TilemapRenderer>().sortingOrder;
+            platformConnectedTo = belowInput.collidingObjects[0];
+            sr.sortingLayerID = platformConnectedTo.GetComponent<TilemapRenderer>().sortingLayerID;
+            sr.sortingOrder = platformConnectedTo.GetComponent<TilemapRenderer>().sortingOrder;
             pairedToPlatform = true;
+
+            platformControllerBelow = platformConnectedTo.GetComponent<PlatformController>();
         }
         else if (belowInput.collidingObjects.Count == 0 && pairedToPlatform)
         {
+            platformConnectedTo = null;
+            platformControllerBelow = null;
             pairedToPlatform = false;
         }
 
+        if (platformControllerBelow != null && passengerrbs.Count > 0)
+        {
+            foreach (Rigidbody2D rb in passengerrbs)
+            {
+                platformControllerBelow.AddPassengerRB(rb);
+            }
+        }
+    }
+    
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Rigidbody2D>() != null)
+        {
+            RemovePassengerRB(collision.gameObject.GetComponent<Rigidbody2D>());
+        }
     }
 
     private void OnDisable()
@@ -51,5 +75,21 @@ public class SpawnedBlockController : FallingObjectController
     private void OnEnable()
     {
         rb.isKinematic = false;
+    }
+
+    public void AddPassengerRB(Rigidbody2D passengerrb)
+    {
+        if (!passengerrbs.Contains(passengerrb))
+        {
+            passengerrbs.Add(passengerrb);
+        }
+    }
+
+    public void RemovePassengerRB(Rigidbody2D passengerrb)
+    {
+        if (passengerrbs.Contains(passengerrb))
+        {
+            passengerrbs.Remove(passengerrb);
+        }
     }
 }
